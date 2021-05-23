5029655241481a47a5e8bd8cdb6439cfae42e15b
Gabriel-tec <brilMiles@gmail.com>
2021-05-23 03:03:28 -0300

    Fórmula de bashkara inserida

--- src/Components/Conteúdo/Fórmulas/Fórmula01.js
+++ src/Components/Conteúdo/Fórmulas/Fórmula01.js
@@ -3,6 +3,47 @@ import './StyleFórmulas/Bashkara.css'
 import {Link} from 'react-router-dom'
 
 class Fórmula01 extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            resultado: '',
+            ValorA: '',
+            ValorB:'',
+            ValorC:''
+
+        }
+    }
+    /* Mudaças de valores*/
+    Valor_A = event =>{
+        this.setState({ValorA: event.target.value})
+    }
+    Valor_B = event =>{
+        this.setState({ValorB: event.target.value})
+    }
+    Valor_C = event =>{
+        this.setState({ValorC: event.target.value})
+    }
+    /*-------------------------- */
+    Calcular = () =>{
+        
+        let A = this.state.ValorA
+        let B = this.state.ValorB
+        let C = this.state.ValorC
+
+        let res = (B*B)-4*A*C
+
+        if(res < 0){
+            res = `Raiz negativa valor "${res}" inválido`
+        }else if (res === 0){
+            res = `Raiz nula "0" inválido`
+        }else{
+            res = `Raiz exata valor "${res}"`
+        }
+
+        this.setState({resultado:res})
+
+    }
     render(){
         return(
             <div>
@@ -13,20 +54,31 @@ class Fórmula01 extends React.Component{
                 <main className='Cálculos'>
                     <div>
                         <p>Valor A:</p>
-                        <input type="number" id="ValorA" />
+                        <input type="number" id="ValorA" 
+                        onChange={this.Valor_A}
+                        />
                     </div>
                     <div>
                         <p>Valor B:</p>
-                        <input type="number" id="ValorB" />
+                        <input type="number" id="ValorB" 
+                        onChange={this.Valor_B}
+
+                        />
                     </div>
                     <div>
                         <p>Valor C:</p>
-                        <input type="number" id="ValorC" />
+                        <input type="number" id="ValorC" 
+                            onChange={this.Valor_C}
+                        />
                     </div>
                     <div>
-                        <button onClick='Calcular()'>Calcular</button>
+                        <button onClick={this.Calcular}>Calcular</button>
                     </div>
-                    <h3 id='Resultado'>Resultado:</h3>
+                    <h3 id='Resultado'>Resultado: ({this.state.resultado})</h3>
+                </main>
+                <main className='tutorial'>
+                    <h2>Como fazer:</h2>
+            
                 </main>
             </div>
         )
